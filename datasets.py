1ab2536957a8fb54500e6e9ca5d34e0f03f20438
Mateusz Grzelinski <grzelinskimat@gmail.com>
2019-09-20 21:34:58 +0200

    WIP add proof of concept of threshold regulator
    Only number of literals can be adjusted, cnf clause is not evaluated properly yet

--- datasets.py
+++ datasets.py
@@ -1,5 +1,4 @@
 from src.exporters.tptp_exporter import TPTPHeader
-from src.generators import WeightedSequence
 from src.generators.factories import FunctorFactory, PredicateFactory, AtomFactory, LiteralFactory, CNFClauseFactory
 from src.generators.randomcnfgenerator import RandomCNFGenerator
 from src.generators.thresholdregulator import ThresholdRegulator
@@ -7,39 +6,29 @@ from src.generators.thresholdregulator import ThresholdRegulator
 
 def dataset1():
     functors = FunctorFactory.generate_functors(names=['f'], arities=[0, 1], max_recursion_depth=1)
-    functors = WeightedSequence.from_weighted_values(functors)
     predicates = PredicateFactory.generate_predicates(names=['p'], arities=[1, 2])
-    predicates = WeightedSequence.from_weighted_values(predicates)
     atoms = AtomFactory.generate_atoms({''})
-    atoms = WeightedSequence.from_weighted_values(atoms)
     literals = LiteralFactory.generate_literals()
-    literals = WeightedSequence.from_weighted_values(literals)
     clauses = CNFClauseFactory.generate_clauses(lengths=[1, 2, 3, 4])
-    clauses = WeightedSequence.from_weighted_values(clauses)
 
     g = RandomCNFGenerator(
-        functors=functors.values,
-        functor_weights=functors.weights,
-        predicates=predicates.values,
-        predicate_weights=predicates.weights,
-        atoms=atoms.values,
-        atom_weights=atoms.weights,
-        literals=literals.values,
-        literal_weights=literals.weights,
-        clauses=clauses.values,
-        clause_weights=clauses.weights
+        functors=functors,
+        predicates=predicates,
+        atoms=atoms,
+        literals=literals,
+        clauses=clauses,
     )
 
-    print(f'{g.functors=}')
-    print(f'{g.predicates=}')
-    print(f'{g.atoms=}')
-    print(f'{g.literals=}')
-    print(f'{g.clauses=}')
+    print(f'{g.functors}')
+    print(f'{g.predicates}')
+    print(f'{g.atoms}')
+    print(f'{g.literals}')
+    print(f'{g.clauses}')
 
-    formula = g.generate_cnf_formula(number_of_clauses=2)
+    formula = g.generate_cnf_formula(number_of_clauses=4)
     tr = ThresholdRegulator(
         number_of_clauses=ThresholdRegulator.range(10, threshold=0.5, delta=5),
-        number_of_literals=ThresholdRegulator.range(10, threshold=0.5, delta=5),
+        number_of_literals=ThresholdRegulator.range(10, threshold=0, delta=2),
         number_of_atoms=ThresholdRegulator.range(10, threshold=0.5, delta=5),
         number_of_predicates=ThresholdRegulator.range(10, threshold=0.5, delta=5),
         number_of_functors=ThresholdRegulator.range(10, threshold=0.5, delta=5),
@@ -52,9 +41,20 @@ def dataset1():
     print(formula)
 
 
+def hash_test():
+    clauses = CNFClauseFactory.generate_clauses(lengths=[1, 2, 3, 4], default_weight=2)
+    clauses_dict = {c.value: c.weight for c in clauses}
+    print(clauses_dict)
+    for c in clauses:
+        print(clauses_dict[c.value])
+        clauses_dict[c.value] = 3
+        print(clauses_dict[c.value])
+
+
 if __name__ == '__main__':
     # f = FunctorFactory.generate_functors(names=['f'], arities=[0, 1, 2], max_recursion_depth=1)
     # f2 = FunctorFactory.generate_liveness_functors(names=['f'])
     # print(f)
     # print(f2)
     dataset1()
+    # hash_test()
