075a48017117cff9194e91e352b8810a23240dd7
Mateusz-gmail <mgrzel@student.agh.edu.pl>
2019-11-01 12:20:40 +0100

    Improve creating tptp header

--- liveness_safety_formula.py
+++ liveness_safety_formula.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from src.ast.first_order_logic import CNFFormula
+from src.ast.exporters.tptp import TPTPHeader
 from src.generators._range import IntegerRange
 from src.generators.presets.first_order_logic import CNFFormulaGenerator
 
@@ -16,12 +16,11 @@ if __name__ == '__main__':
     )
     formula_gen = gen.generate()
     formula = next(formula_gen)
-    formula = next(formula_gen)
-    formula: CNFFormula
-    # header = TPTPHeader()
-    # header.read_from(formula)
-    # print(header.get_header())
-    print('stats: ')
+    print('Currently supported statistics: ')
     pprint(formula.get_info().__dict__)
+
     print('generated formula:')
+    header = TPTPHeader()
+    header.read_from(formula.get_info())
+    print(header.get_header())
     pprint(formula)
