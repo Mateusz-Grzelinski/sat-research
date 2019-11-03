9a4481c2ce013940d19dd32be982053e9eef12c8
Mateusz-gmail <mgrzel@student.agh.edu.pl>
2019-11-03 13:06:15 +0100

    Ensure that generated fol elements are unique

--- liveness_safety_formula.py
+++ liveness_safety_formula.py
@@ -6,12 +6,12 @@ from src.generators.presets.first_order_logic import CNFFormulaGenerator
 
 if __name__ == '__main__':
     gen = CNFFormulaGenerator(
-        functor_names={'f1'}, functor_arity={0}, functor_recursion_depth=0,
-        predicate_names={'p1', 'p2'}, predicate_arities={0, 1},
+        functor_names={f'f{i}' for i in range(10)}, functor_arity={0}, functor_recursion_depth=0,
+        predicate_names={f'p{i}' for i in range(10)}, predicate_arities={0, 1},
         connectives={''},
         clause_lengths={1, 2, 3},
-        variable_names={'V1', 'V2'},
-        number_of_clauses=IntegerRange(min=3, max=10),
+        variable_names={f'V{i}' for i in range(10)},
+        number_of_clauses=IntegerRange(min=3, max=100),
         number_of_literals=IntegerRange(min=1, max=30)
     )
     formula_gen = gen.generate()
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     print('Currently supported statistics: ')
     pprint(formula.get_info().__dict__)
 
-    print('generated formula:')
+    print('Generated formula:')
     header = TPTPHeader()
     header.read_from(formula.get_info())
     print(header.get_header())
