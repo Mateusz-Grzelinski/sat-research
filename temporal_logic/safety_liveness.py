15066cd1c67cc67dd9a5f63338f60f3daa168653
Mateusz Grzelinski <grzelinskimat@gmail.com>
2019-12-21 14:51:25 +0100

    Add random logical connective to temporal logic formu

--- temporal_logic/safety_liveness.py
+++ temporal_logic/safety_liveness.py
@@ -6,10 +6,12 @@ from src.generators.presets.propositional_temporal_logic.propositional_temporal_
 sys.setrecursionlimit(4000)
 
 if __name__ == '__main__':
-    gen = PropositionalTemporalLogicGenerator(variable_names={f'V{i}' for i in range(10)},
-                                              number_of_variables_with_always_connectives=5,
-                                              number_of_variables_with_eventually_connectives=5,
-                                              number_of_variables_with_both_connectives=5,
-                                              number_of_variables_without_connective=5)
+    gen = PropositionalTemporalLogicGenerator(
+        variable_names={f'V{i}' for i in range(10)},
+        number_of_variables_with_always_connectives=0,
+        number_of_variables_with_eventually_connectives=1,
+        number_of_variables_with_both_connectives=0,
+        number_of_variables_without_connective=10,
+        logical_connectives={'|', '&'})
     formula = gen.generate()
     print(str(formula))
