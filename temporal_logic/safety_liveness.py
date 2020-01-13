2c0fe65cb592c31b5754bea9e90ee4eea6ee5162
Mateusz Grzelinski <czubaka111@gmail.com>
2020-01-13 23:51:34 +0100

    Add CNF PTL formula generator

--- temporal_logic/safety_liveness.py
+++ temporal_logic/safety_liveness.py
@@ -1,17 +1,27 @@
 import sys
 
-from src.generators.presets.propositional_temporal_logic.propositional_temporal_logic_generator import \
-    PropositionalTemporalLogicGenerator
+from src.ast.exporters.inkresat.inkresat_exporter import InkresatExporter
+from src.generators import IntegerRange
+from src.generators.presets.propositional_temporal_logic.cnf_propositional_temporal_logic_generator import \
+    CNFPropositionalTemporalLogicGenerator
 
 sys.setrecursionlimit(4000)
 
 if __name__ == '__main__':
-    gen = PropositionalTemporalLogicGenerator(
+    system_rpoperty_gen = CNFPropositionalTemporalLogicGenerator(
         variable_names={f'V{i}' for i in range(10)},
-        number_of_variables_with_always_connectives=0,
-        number_of_variables_with_eventually_connectives=1,
-        number_of_variables_with_both_connectives=0,
-        number_of_variables_without_connective=10,
-        logical_connectives={'|', '&'})
-    formula = gen.generate()
-    print(str(formula))
+        number_of_variables_without_connective=IntegerRange(0, 0),
+        number_of_variables_with_always_connectives=IntegerRange(1, 3),
+        number_of_variables_with_eventually_connectives=IntegerRange(1, 3),
+        number_of_clauses=IntegerRange(2, 10),
+        clause_lengths={1})
+    exporter = InkresatExporter(
+        output_dir='./test-inkresat-cnf/',
+        filename_handle=lambda formula_info: '',
+        statistics_to_file=True
+    )
+    for i, formula in enumerate(system_rpoperty_gen.generate()):
+        print(str(formula))
+        print(formula.get_info())
+        exporter.export(formula, filename_suffix=str(i))
+        break
